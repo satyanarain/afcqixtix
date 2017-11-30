ff66442eced539d61c1d91cab0c3a34318fcf015
opiantkunal <kunal_kumar@opiant.in>
2017-11-30 18:07:33 +0530

    pata nahi kya kiya

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,14 +1,15 @@
 <?php
 namespace App\Http\Controllers\Auth;
+
+use DB;
+use Auth;
 use App\Http\Requests;
+use App\Models\Chatter;
+use Illuminate\Http\Request;
+use Illuminate\Http\Respons;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
-use App\Models\Chatter;
-use Illuminate\Http\Respons;
-//use \Carbon\Carbon; 
-use Auth;
-use DB;
+
 class LoginController extends Controller
 {
     /*
@@ -40,48 +41,19 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-     public function login(Request $request)
+    
+    public function login(Request $request)
     {
-    $this->validate($request, ['email' => 'required|email', 'password' => 'required']);
-    $email=$request['email'];
-    echo "<pre>";
- 
-   $test= Auth::attempt(['email' => $request['email'], 'password' => $request['password']]);
+        $this->validate($request, ['email' => 'required|email', 'password' => 'required']);
+        Auth::attempt(['email' => $request['email'], 'password' => $request['password']]);
    
-      if(!Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
-        return redirect()->back()->with('fail', 'Either username or password is incorrect!');
+        if(!Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
+            return redirect()->back()->with('fail', 'Either username or password is incorrect!');
         }else
         { 
-
-         $sql = DB::table('users')->select('*')->where('email','=',$email)->first();
-          $id=$sql->id;
-         $name=$sql->name;
-         $seen = date('Y:m:d H:i:s');
-         DB::update("update users set logged_user = 1 where email = ?", [$email]);
-//
-//        $sql = DB::table('chatters')->select('*')->where('user_id','=',$id)->first();
-//
-//       if(count($sql)==0)
-//       {
-//         $chatter= Chatter::create(['user_id'=>$id,'name'=>$name,'seen'=>$seen]);
-//         $chatter->save();
-//       }
-          return redirect('/dashboard');
-         }
+            return redirect('/dashboard');
+        }
 
    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
