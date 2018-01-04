4cb82d85dacf6b4d2864a8188f0858e726d671fd
opiantkunal <kunal_kumar@opiant.in>
2018-01-04 12:22:25 +0530

    dhus

--- database/migrations/2017_12_14_090809_create_vehicles_table.php
+++ database/migrations/2017_12_14_090809_create_vehicles_table.php
@@ -15,9 +15,11 @@ class CreateVehiclesTable extends Migration
     {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->increments('id');
-             $table->integer('user_id')->unsigned();
-         $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('depot_id')->references('id')->on('depot');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('depot_id')->unsigned();
+            $table->foreign('depot_id')->references('id')->on('depots');
+            $table->integer('bus_type_id')->unsigned();
             $table->foreign('bus_type_id')->references('id')->on('bus_types');
             $table->integer('vehicle_registration_number');
             $table->string('bus_type');
