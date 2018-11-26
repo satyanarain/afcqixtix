16420c217e158774f772f1b4aaf03aea21342bba
satyanarain <satyanarain_chauhan@opaint.in>
2018-11-26 16:46:57 +0530

    26-11-18

--- app/Models/EtmDetail.php
+++ app/Models/EtmDetail.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
-class EtmDetail extends Model
+class ETMDetail extends Model
 {
   /**
      * The database table used by the model.
