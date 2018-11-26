acc486397d12a74b35c47c1f4a42358aade5bc89
satyanarain <satyanarain_chauhan@opaint.in>
2018-11-26 11:27:59 +0530

    26-11-18

--- resources/views/waybills/closeform.blade.php
+++ resources/views/waybills/closeform.blade.php
@@ -145,20 +145,25 @@
                 <td>ETM No. / Abstract No.</td>
                 <td>Route / Duty / Shift</td>
                 <td>Vehicle-Bus Type</td>
-                <td>ATM Card / e-purse (Rs.)</td>
-                <td>Cash (Rs.)</td>
+                <td>ETM pass / Ticket</td>
                 <td>Payout (Rs.)</td>
                 <td>Audit</td>
             </tr>
             <tr>
-                <td>12 05:52
-12 13:38</td>
+                <td><?php   if($shift_details->start_timestamp){echo date('H:i',strtotime($shift_details->start_timestamp)).'<br>';}
+                            if($shift_details->end_timestamp){echo date('H:i',strtotime($shift_details->end_timestamp));}?></td>
+                <td><?php echo $shift_details->conductor_name.'<br>'.$shift_details->driver_name;?></td>
+                <td><?php echo $shift_details->abstract_no;?></td>
+                <td><?php echo $shift_details->route.'/'.$shift_details->duty_number.'/'.$shift_details->shift;?></td>
+                <td><?php echo $shift_details->vehicle_registration_number.'-'.$shift_details->bus_type;?></td>
+                <td style="text-align: right"><?php echo $etm_pass_amount.'<br>'.$etm_ticket_amount;?></td>
+                <td style="text-align: right"><?php echo $total_payout;?></td>
                 <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+            </tr>
+            <tr>
+                <td colspan="5">Total</td>
+                <td style="text-align: right"><?php echo $etm_pass_amount+$etm_ticket_amount;?></td>
+              
                 <td></td>
                 <td></td>
             </tr>
@@ -182,14 +187,33 @@
                 <td>No. of Tkt. Sold</td>
                 <td>Total Value (Rs.)</td>
             </tr>
+            <?php $count=1;
+                foreach($items as $item){
+                    echo '<tr>
+                    <td>'.$item->name.'</td>
+                    <td>'.$item->denom.'</td>
+                    <td>'.$item->series.'</td>
+                    <td>'.$item->start_sequence.'</td>
+                    <td>';
+                    if($item->end_sequence){
+                        echo '<input name="ticketstock-'.$item->id.'-'.$item->denomination_id.'" value="0" min="'.$item->start_sequence.'" price="'.$item->price.'" count="'.$count.'" max="'.$item->end_sequence.'" class="audit_items" type="text" style="width:50px;height:15px;">'.$item->end_sequence;
+                    }else{
+                        echo '<input name="item-stock-'.$item->id.'" value="0" min="'.$item->quantity.'" price="0" count="'.$count.'" max="'.$item->quantity.'" class="audit_items" type="text" style="width:50px;height:15px;">'.$item->quantity;
+                    }
+                    echo '</td>
+                    <td style="text-align: right" class="ticket_sold_count'.$count.'">0</td>
+                    <td style="text-align: right" class="ticket_sold_value'.$count.'">0</td>
+            </tr>';
+            $count++;
+            }?>
             <tr>
+                <td><b>Total</b></td>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
-                <td></td>
-                <td></td>
+                <td style="text-align: right" class="total_ticket_value">0</td>
             </tr>
         </table>
     </div>
@@ -200,7 +224,55 @@
     <div class="col-md-12 col-sm-12 alert-danger cash-collection-error hide"></div>
     <h3 class="box-title">Remittance Details</h3>
 </div>
+<div class="form-group ">
+     <div class="col-md-2 col-sm-12 control-label">
+         Total Cash (Rs.)
+    </div>
+    <div class="col-md-4 col-sm-12 total_cash">
+         <?php echo $etm_ticket_amount+$etm_pass_amount;?>
+    </div>
+    <div class="col-md-2 col-sm-12 control-label">
+        Total Payout (Rs.)
+    </div>
+    <div class="col-md-4 col-sm-12 total_payout">
+         <?php echo $total_payout;?>
+    </div>
 
+     
+</div> 
+<div class="form-group ">
+     {!! Form::label('batta', Lang::get('Batta (Rs.)'), ['class' => 'col-md-2 control-label']) !!}
+    <div class="col-md-4 col-sm-12">
+          {!! Form::text('batta', 0, ['class' => 'col-md-6 form-control batta']) !!}
+    </div>
+
+     {!! Form::label('driver_incentive', Lang::get('Driver Incentive (Rs.)'), ['class' => 'col-md-2 control-label']) !!}
+    <div class="col-md-4 col-sm-12">
+          {!! Form::text('driver_incentive', 0, ['class' => 'col-md-6 form-control driver_incentive']) !!}
+    </div>
+</div>
+<div class="form-group ">
+     {!! Form::label('conductor_incentive', Lang::get('Conductor Incentive (Rs.)'), ['class' => 'col-md-2 control-label']) !!}
+    <div class="col-md-4 col-sm-12">
+          {!! Form::text('conductor_incentive', 0, ['class' => 'col-md-6 form-control conductor_incentive']) !!}
+    </div>
+
+     {!! Form::label('tea_allowance', Lang::get('Tea Allowance (Rs.)'), ['class' => 'col-md-2 control-label']) !!}
+    <div class="col-md-4 col-sm-12">
+          {!! Form::text('tea_allowance', 0, ['class' => 'col-md-6 form-control tea_allowance']) !!}
+    </div>
+</div>
+<div class="form-group ">
+     {!! Form::label('payable_amount', Lang::get('Payable Amount (Rs.)'), ['class' => 'col-md-2 control-label']) !!}
+    <div class="col-md-4 col-sm-12 payable_amount">
+          <?php echo $etm_ticket_amount+$etm_pass_amount-$total_payout;?>
+    </div>
+
+     {!! Form::label('remarks', Lang::get('Remarks'), ['class' => 'col-md-2 control-label']) !!}
+    <div class="col-md-4 col-sm-12">
+          {!! Form::text('remarks', null, ['class' => 'col-md-6 form-control']) !!}
+    </div>
+</div>
 <div class="form-group">
     <div class="col-md-3" style="margin-right: 15px;"></div>
     {{ Form::submit('Save', array('class' => 'btn btn-success pull-left','required' => 'required')) }}
@@ -227,5 +299,47 @@
         else
             $('.actual_driver_id').hide();
     })
+    $(document).on("change", ".audit_items", function() {
+        var sum = parseInt($(".total_ticket_value").html());
+        var total_cash = parseInt($(".total_cash").html());
+        var payable_amount = parseInt($(".payable_amount").html());
+        var min = parseInt($(this).attr('min'));
+        var max = parseInt($(this).attr('max'));
+        var curr_val = parseInt($(this).val());
+        var counter = $(this).attr('count');
+        var price = $(this).attr('price');
+        if(parseInt(price)==0)
+            return false;
+        if(curr_val<min || curr_val>max)
+        {
+            alert('invalid number');
+            $(this).val(0);
+            $(this).focus()
+            return false;
+        }else{
+            var ticketcount = curr_val-min;
+            var ticketvalue = ticketcount*price;
+            $(".ticket_sold_count"+counter).html(ticketcount);
+            $(".ticket_sold_value"+counter).html(ticketvalue);
+            sum += ticketvalue;
+            total_cash+= ticketvalue;
+            payable_amount += ticketvalue;
+        }
+        $(".total_ticket_value").html(sum);
+        $(".total_cash").html(total_cash);
+        $(".payable_amount").html(payable_amount);
+    });
+    
+    $(document).on("change", ".batta, .driver_incentive, .conductor_incentive, .tea_allowance", function() {
+        var payable_amount = parseInt($(".total_cash").html())-parseInt($(".total_payout").html());
+        var batta = parseInt($(".batta").val());
+        var driver_incentive = parseInt($(".driver_incentive").val());
+        var conductor_incentive = parseInt($(".conductor_incentive").val());
+        var tea_allowance = parseInt($(".tea_allowance").val());
+        var updated_payable_amount = payable_amount-batta-driver_incentive-conductor_incentive-tea_allowance;
+        $(".payable_amount").html(updated_payable_amount);
+    });
+    
+
 })</script>
 @endpush
