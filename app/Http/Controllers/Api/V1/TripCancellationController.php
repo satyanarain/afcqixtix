1f2fb1b2b297bfbf653dda63285161ec05f2a4ae
kishan kumar <kishan_kumar@opiant.in>
2018-12-28 12:55:49 +0530

    modified trip cancellation api

--- app/Http/Controllers/Api/V1/TripCancellationController.php
+++ app/Http/Controllers/Api/V1/TripCancellationController.php
@@ -13,29 +13,40 @@ class TripCancellationController extends Controller
     public function store(Request $request)
     {
     	$validator = Validator::make($request->all(), [
-    		'cancellation_timestamp' => 'required',
-            'abstract_no' => 'required',
-            'trip_no' => 'required',
-            'reason_id' => 'required'
-    	]);
+            'data' => 'required'
+        ]);
 
-    	if($validator->fails())
-    	{
-    		return response()->json(['statusCode'=>'Error', 'errorCode'=>'VALIDATION', 'data'=>$validator->errors()]);
-    	}
+        if($validator->fails())
+        {
+            return response()->json(['statusCode'=>'Error', 'errorCode'=>'VALIDATION', 'data'=>$validator->errors()]);
+        }
 
-    	//check if abstract number is available in waybills
+        $jsonDecoded = json_decode($request->data, true);
 
-    	$waybill = Waybill::where('abstract_no', $request->abstract_no)
-    						->whereDate('date', date('Y-m-d', strtotime($request->cancellation_timestamp)))
-    						->first();
-    	if($waybill)
-    	{
-    		$tripStart = TripCancellation::create(['abstract_no'=>$request->abstract_no, 'trip_no'=>$request->trip_no, 'reason_id'=>$request->reason_id, 'cancellation_timestamp'=>$request->cancellation_timestamp]);
+        $insertedData = [];
 
-    		return response()->json(['statusCode'=>'Ok', 'data'=>'Trip Cancelled Successfully.']);
-    	}else{
-    		return response()->json(['statusCode'=>'Error', 'errorCode'=>'INVALID_ABSTRACT', 'data'=>'Invalid abstract number.']);
-    	}
+        foreach ($jsonDecoded as $key => $value) 
+        {
+            try{
+                //check if abstract number is available in waybills
+
+                $waybill = Waybill::where('abstract_no', $value['abstract_no'])
+                                    ->whereDate('date', date('Y-m-d', strtotime($value['cancellation_timestamp'])))
+                                    ->first();
+                if($waybill)
+                {
+                    $tripStart = TripCancellation::create(['abstract_no'=>$value['abstract_no'], 'trip_no'=>$value['trip_no'], 'reason_id'=>$value['reason_id'], 'cancellation_timestamp'=>$value['cancellation_timestamp'], 'stop_id'=>$value['stop_id']]);
+
+                    $insertedData[] = ['id'=>$value['local_id'], 'status'=>1];
+
+                }else{
+                    $insertedData[] = ['id'=>$value['local_id'], 'status'=>0];
+                }
+            } catch (\Illuminate\Database\QueryException $exception) {
+                $insertedData[] = ['id'=>$value['local_id'], 'status'=>0];
+            }            
+        }
+
+        return response()->json(['statusCode'=>'Ok', 'data'=>$insertedData]);
     }
 }
